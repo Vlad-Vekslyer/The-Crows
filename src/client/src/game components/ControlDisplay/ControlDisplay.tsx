c917004ee70d424546f0e0917d4be00b9dff9c4d
unknown <vladi00345@gmail.com>
2019-10-08 00:48:05 -0700

    Implemented Control Display

--- src/client/src/game components/ControlDisplay/ControlDisplay.tsx
+++ src/client/src/game components/ControlDisplay/ControlDisplay.tsx
@@ -1,12 +1,25 @@
 import React from "react"
 import {sizes} from "../style"
 import styled from "styled-components"
+import ControlCounter from "../../assets/ControlCounter.png"
 
 const StyledControl = styled.span`
   align-self: flex-end;
+  text-align: center;
+  width: 80px;
+  height: 52px;
+  font-size: 3em
+  font-family: Typewriter;
+  background-image: url(${ControlCounter});
+  background-position: center;
+  background-size: cover;
   @media (max-width:${sizes.medium}px){
     align-self: inherit;
   }`
+
+const StyledNumber = styled.span`
+  display: inline-block;
+  margin-top: 6px;`
 
 interface Props{
   control: number
@@ -15,7 +28,7 @@ interface Props{
 function ControlDisplay(props: Props){
   return(
     <StyledControl>
-      <span>Control: </span><span>{props.control}</span>
+      <StyledNumber>{props.control}</StyledNumber>
     </StyledControl>
   )
 }
